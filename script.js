c50057f2145114b8f06082a1de652dc062f3b64b
SharMi <sivabalaa8@gmail.com>
2026-01-12 14:34:54 +0530

    new resume

--- script.js
+++ script.js
@@ -1104,7 +1104,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function downloadResume() {
   // Path to your PDF (inside assets folder)
-  const resumeUrl = './assets/Siva%20Resume.pdf';  // %20 = space
+  const resumeUrl = './assets/Siva%20ResumeNew.pdf';  // %20 = space
 
   // Create invisible <a> tag
   const link = document.createElement('a');
